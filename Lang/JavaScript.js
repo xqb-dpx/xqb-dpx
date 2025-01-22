33bb40cb11b1a9aeddacdaccae3cb9bfc4132784
b‿daarr <b.daarr@outlook.com>
2025-01-22 10:00:08 +0330

    Update Lang

---
+++ Lang/JavaScript.js
@@ -0,0 +1,9 @@
+/* b‿daarr */
+// Quasar
+
+document.addEventListener('DOMContentLoaded', () => {
+    const projects = document.querySelector('h1');
+    projects.forEach(projects => {
+        console.log(projects.className);
+    });
+});
