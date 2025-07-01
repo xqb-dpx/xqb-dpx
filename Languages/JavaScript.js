d9bd301e4a4da08d499a9932508b82b7dd5f1b76
b‿daarr <b.daarr@outlook.com>
2025-07-01 18:53:24 +0330

    Update JavaScript.js

--- Languages/JavaScript.js
+++ Languages/JavaScript.js
@@ -1,9 +1,14 @@
-// Java
-/* java */
+// JavaScript
+/* js */
 
-document.addEventListener('DOMContentLoaded', () => {
-    const projects = document.querySelector('h1');
-    projects.forEach(projects => {
-        console.log(projects.className);
-    });
+const button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomColor();
 });
+
+function getRandomColor() {
+    const colors = ['#f39c12', '#1abc9c', '#9b59b6'];
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
